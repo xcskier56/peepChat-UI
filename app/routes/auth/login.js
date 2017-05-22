472930723f31d8fa6a355a05739c035f5c8823f8
Doug DeBold <ddebold56@gmail.com>
2017-05-22 13:37:16 -0500

    added flash notifications to app

--- app/routes/auth/login.js
+++ app/routes/auth/login.js
@@ -4,19 +4,39 @@ const { Route, inject } = Ember;
 
 export default Route.extend({
   session: inject.service(),
-  actions: {
-    doLogin() {
-      const user = this.get('currentModel');
-      this.get('session').authenticate(
-        'authenticator:peepchat', user.email, user.password
-      );
-    }
-  },
+  flashMessages: inject.service(),
 
   model() {
     return {
       email: '',
       password: ''
     };
+  },
+
+  actions: {
+    doLogin() {
+      const user = this.get('currentModel');
+      this.get('session').authenticate(
+        'authenticator:peepchat',
+        user.email,
+        user.password
+      ).then(() => {
+        // Successful Login
+        this.get('flashMessages').success('Logged in!');
+      }).catch((response) => {
+        const { errors } = response;
+
+        // Check if any errors have a 401 code
+        if (errors.mapBy('code').indexOf(401) >= 0) {
+          // Unauthorized
+          this.get('flashMessages')
+            .danger('There was a problem with your username or password, please try again');
+
+        } else {
+          // All other API errors
+          this.get('flashMessages').danger('Server Error');
+        }
+      });
+    }
   }
 });
